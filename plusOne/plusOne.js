3b3ddad9609dcd25c63f291966b5a24980f1e25a
JuanCeja <jmc347ls2@yahoo.com>
2022-07-21 17:05:03 -0700

    completed

--- plusOne/plusOne.js
+++ plusOne/plusOne.js
@@ -34,13 +34,16 @@ let digits1 = [4,3,2,1];
 let digits2 = [9];
 
 var plusOne = function(digits) {
-    let number = "";
-    for(let i = 0; i < digits.length; i++){
-        number = number.concat(digits[i]);
-        // console.log(number);
+    for  (let i = digits.length - 1; i >= 0; i--){
+        if (digits[i] < 9) {
+            digits[i] = digits[i] + 1;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
     }
-
-    
+    digits.unshift(1);
+    return digits;
 };
 
 console.log(plusOne(digits));
