a5bc94b9fc3a55933dfa16a7a5e860695818282c
JuanCeja <jmc347ls2@yahoo.com>
2022-07-22 14:05:09 -0700

    completed

--- addBinary/addBinary.js
+++ addBinary/addBinary.js
@@ -19,8 +19,24 @@ b = "1";
 a1 = "1010";
 b1 = "1011"
 
-var addBinary = function(a, b) {
-    
+var addBinary = function (a, b) {
+    let carry = 0;
+    let result = "";
+
+    let len1 = a.length - 1;
+    let len2 = b.length - 1;
+
+    for(; len1 >= 0 || len2 >= 0 || carry > 0; len1--, len2--){
+        let sum = (+a[len1] || 0) + (+b[len2] || 0) + carry;
+        if(sum > 1){
+            sum = sum % 2;
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+        result = `${sum}${result}`;
+    }
+    return result;
 };
 
 console.log(addBinary(a, b));
@@ -28,3 +44,4 @@ console.log(addBinary(a, b));
 
 console.log(addBinary(a1, b1));
 // Output: "10101"
+
