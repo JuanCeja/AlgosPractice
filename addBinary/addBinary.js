bd27b778530d839928a6214836e432f30251c49c
JuanCeja <jmc347ls2@yahoo.com>
2022-07-22 14:05:00 -0700

    initial commit

---
+++ addBinary/addBinary.js
@@ -0,0 +1,30 @@
+// Given two binary strings a and b, return their sum as a binary string.
+
+
+// Example 1:
+
+// Input: a = "11", b = "1"
+// Output: "100"
+
+// Example 2:
+
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+
+// ====================================================================================================
+
+a = "11";
+b = "1";
+
+a1 = "1010";
+b1 = "1011"
+
+var addBinary = function(a, b) {
+    
+};
+
+console.log(addBinary(a, b));
+// Output: "100"
+
+console.log(addBinary(a1, b1));
+// Output: "10101"
