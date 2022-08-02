81563531b1f737ed00fa2aa6d9e5e45c89ab8b2f
JuanCeja <jmc347ls2@yahoo.com>
2022-08-02 14:39:18 -0700

    refactoring using binary search

--- searchInsertPosition-ABM!!/searchInsertPosition.js
+++ searchInsertPosition-ABM!!/searchInsertPosition.js
@@ -30,7 +30,20 @@ let nums2 = [1,3,5,6];
 let target2 = 7;
 
 var searchInsert = function(nums, target) {
+    let left = 0
+    let right = nums.length;
     
+    while(left < right){
+        let mid = 1 + Math.floor((right - left) / 2);
+
+        if(mid > target){
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+
+    return left + 1;
 };
 
 console.log(searchInsert(nums, target));
@@ -44,39 +57,3 @@ console.log(searchInsert(nums1, target1));
 console.log(searchInsert(nums2, target2));
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for ( let i = 0; i < nums.length; i++){
-//     if(nums[i] === target) return i;
-//     if(nums[0] > target) return 0;
-//     if(nums[i] < target && nums[i+1] > target) return i + 1;
-//     if(nums[nums.length - 1] < target) return nums.length;
-// }
