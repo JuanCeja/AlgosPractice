3438254c28a2dc5b5121e4feac28a261ab19f0ca
JuanCeja <jmc347ls2@yahoo.com>
2022-08-03 16:21:42 -0700

    finished solution

--- rotateArray/rotateArray.js
+++ rotateArray/rotateArray.js
@@ -24,19 +24,27 @@ let nums = [1,2,3,4,5,6,7];
 let k = 3;
 
 let nums2 = [-1,-100,3,99]; 
-let k2 = 2
-
+let k2 = 2;
 
 var rotate = function(nums, k){
     k = k % nums.length;
 
-    while(k > 0){
-        nums.unshift(nums.pop());
-        k--;
-    }
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
 
     return nums;
 }
+
+var reverse = function(nums, start, end){
+    while(start < end){
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+};
 
 console.log(rotate(nums, k));
 // Output: [5,6,7,1,2,3,4]
