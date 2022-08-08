ea1331cd9a562e542b7487b15dd472393c015f68
JuanCeja <jmc347ls2@yahoo.com>
2022-08-08 14:32:59 -0700

    added step by step comments

--- reverseWordsInAString3/reverseWordsInAStringIII.js
+++ reverseWordsInAString3/reverseWordsInAStringIII.js
@@ -19,13 +19,16 @@ let s = "Let's take LeetCode contest";
 let s2 = "God Ding";
 
 var reverseWords = function(s){
+    // step 1: tokenize into words
     let string = s.split(' ');
 
+    // step 2: reverse each word
     for(let i = 0; i < string.length; i++){
         let word = string[i].split('');
         let reversedWord = word.reverse().join('');
         string[i] = reversedWord;
     }
+    // step 3: join the reversed words back to a new sentence
     return string.join(' ');
 }
 
