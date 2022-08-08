17ef94dc7dce4afb13660d237a846ad9693c1e84
JuanCeja <jmc347ls2@yahoo.com>
2022-08-08 15:56:35 -0700

    removed .split()

--- findNumbersWithEvenNumbersOfDigits/findNumbersWithEvenNumbersOfDigits.js
+++ findNumbersWithEvenNumbersOfDigits/findNumbersWithEvenNumbersOfDigits.js
@@ -29,7 +29,7 @@ var findNumbers = function(nums){
     let count = 0;
 
     for(let i = 0; i < nums.length; i++){
-        let length = nums[i].toString().split('').length
+        let length = nums[i].toString().length
         
         if(length % 2 === 0){
             count++;
