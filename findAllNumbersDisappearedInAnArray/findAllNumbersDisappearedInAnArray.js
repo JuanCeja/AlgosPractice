8b3108d24469456fa42d3b512a5d043d42a89344
JuanCeja <jmc347ls2@yahoo.com>
2022-08-18 16:44:07 -0700

    complete

--- findAllNumbersDisappearedInAnArray/findAllNumbersDisappearedInAnArray.js
+++ findAllNumbersDisappearedInAnArray/findAllNumbersDisappearedInAnArray.js
@@ -17,7 +17,17 @@ let nums = [4,3,2,7,8,2,3,1];
 let nums2 = [1,1];
 
 var findDisappearedNumbers = function(nums) {
-    
+    let missingNumbers = [];
+
+    for(let i = 0; i < nums.length; i++){
+        let j = Math.abs(nums[i]) - 1;
+        nums[j] = Math.abs(nums[j]) * -1;
+    }
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > 0) missingNumbers.push(i + 1);
+    }
+    return missingNumbers;
 }
 
 
